3570f9995cba2737b95524a7b37550dffd59a1ad
Pedro Breit <pe.breit@duocuc.cl>
2026-07-11 16:25:23 -0400

    feat: integra Oracle Autonomous Database

--- cursos-service/src/test/java/com/duoc/cursos/CursosServiceApplicationTests.java
+++ cursos-service/src/test/java/com/duoc/cursos/CursosServiceApplicationTests.java
@@ -2,12 +2,13 @@ package com.duoc.cursos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CursosServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
